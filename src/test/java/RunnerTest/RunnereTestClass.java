69f4a3baa6e2fe942894ca8456fbb747e04e8668
RSC <bhagyashri22jadhav@gmail.com>
2025-01-24 22:00:09 +0000

    RunnerTest class is created by bhagyashri

--- src/test/java/RunnerTest/RunnereTestClass.java
+++ src/test/java/RunnerTest/RunnereTestClass.java
@@ -1,5 +1,18 @@
 package RunnerTest;
 
-public class RunnereTestClass {
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+		
+		features="C:\\Users\\RSC\\git\\Facebook\\Features",
+		glue="StepDefination",
+		dryRun=true,
+		monochrome=true
+		
+		)
+
+
+public class RunnereTestClass extends AbstractTestNGCucumberTests{
 
 }
